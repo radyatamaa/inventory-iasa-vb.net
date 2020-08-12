06269dc3e9da90535b79c78290cce5318df8a909
bagusshndr <bagussuhendri19@gmail.com>
2020-08-12 19:58:14 +0700

    push design barang masuk 12 agustus terbaru, dan buat data tampung

--- Inventaris/BarangKeluar.vb
+++ Inventaris/BarangKeluar.vb
@@ -1,76 +1,78 @@
 ﻿Public Class BarangKeluar
-    Private Sub btn_menuutama_Click(sender As Object, e As EventArgs) Handles btn_menuutama.Click
+    Private Sub btn_menuutama_Click(sender As Object, e As EventArgs)
         MenuUtama.Show()
         Me.Close()
     End Sub
-    Private Sub btn_barang_masuk_Click(sender As Object, e As EventArgs) Handles btn_barang_masuk.Click
+    Private Sub btn_barang_masuk_Click(sender As Object, e As EventArgs)
         BarangMasuk.Show()
         Me.Close()
     End Sub
-    Private Sub btn_rental_Click(sender As Object, e As EventArgs) Handles btn_rental.Click
+    Private Sub btn_rental_Click(sender As Object, e As EventArgs)
         Rental.Show()
         Me.Close()
     End Sub
 
-    Private Sub btn_repair_Click(sender As Object, e As EventArgs) Handles btn_repair.Click
+    Private Sub btn_repair_Click(sender As Object, e As EventArgs)
         Repair.Show()
         Me.Close()
     End Sub
 
-    Private Sub btn_master_jenis_Click(sender As Object, e As EventArgs) Handles btn_master_jenis.Click
+    Private Sub btn_master_jenis_Click(sender As Object, e As EventArgs)
         MasterJenisBarang.Show()
         Me.Close()
     End Sub
 
-    Private Sub btn_master_tipe_Click(sender As Object, e As EventArgs) Handles btn_master_tipe.Click
+    Private Sub btn_master_tipe_Click(sender As Object, e As EventArgs)
         MasterTipe.Show()
         Me.Close()
     End Sub
 
-    Private Sub btn_master_toko_Click(sender As Object, e As EventArgs) Handles btn_master_toko.Click
+    Private Sub btn_master_toko_Click(sender As Object, e As EventArgs)
         MasterDataToko.Show()
         Me.Close()
     End Sub
 
-    Private Sub btn_master_client_Click(sender As Object, e As EventArgs) Handles btn_master_client.Click
+    Private Sub btn_master_client_Click(sender As Object, e As EventArgs)
         MasterClient.Show()
         Me.Close()
     End Sub
 
-    Private Sub btn_master_kondisi_Click(sender As Object, e As EventArgs) Handles btn_master_kondisi.Click
+    Private Sub btn_master_kondisi_Click(sender As Object, e As EventArgs)
         MasterKondisi.Show()
         Me.Close()
     End Sub
 
-    Private Sub btn_master_status_Click(sender As Object, e As EventArgs) Handles btn_master_status.Click
+    Private Sub btn_master_status_Click(sender As Object, e As EventArgs)
         MasterStatus.Show()
         Me.Close()
     End Sub
 
-    Private Sub MasterLokasiToolStripMenuIbtn_master_lokasi_Click(sender As Object, e As EventArgs) Handles MasterLokasiToolStripMenuIbtn_master_lokasi.Click
+    Private Sub MasterLokasiToolStripMenuIbtn_master_lokasi_Click(sender As Object, e As EventArgs)
         MasterLokasi.Show()
         Me.Close()
     End Sub
 
-    Private Sub btn_master_detail_Click(sender As Object, e As EventArgs) Handles btn_master_detail.Click
+    Private Sub btn_master_detail_Click(sender As Object, e As EventArgs)
         MasterDetailLokasi.Show()
         Me.Close()
     End Sub
 
-    Private Sub btn_master_alasan_Click(sender As Object, e As EventArgs) Handles btn_master_alasan.Click
+    Private Sub btn_master_alasan_Click(sender As Object, e As EventArgs)
         MasterAlasan.Show()
         Me.Close()
     End Sub
 
-    Private Sub btn_master_data_user_Click(sender As Object, e As EventArgs) Handles btn_master_data_user.Click
+    Private Sub btn_master_data_user_Click(sender As Object, e As EventArgs)
         MasterUser.Show()
         Me.Close()
     End Sub
 
-    Private Sub btn_logout_Click(sender As Object, e As EventArgs) Handles btn_logout.Click
+    Private Sub btn_logout_Click(sender As Object, e As EventArgs)
         Form1.Show()
         Me.Close()
     End Sub
 
+    Private Sub data_barang_penjualan_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles data_barang_penjualan.CellContentClick
 
+    End Sub
 End Class
