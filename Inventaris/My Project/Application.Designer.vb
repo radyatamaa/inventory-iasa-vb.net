128bb2fc30db632b47ea3cc0d81ae3369ff32022
User <User@DESKTOP-CAEBGTR>
2020-08-14 03:02:30 +0700

    implement CRUD Master Tipe Barang

--- Inventaris/My Project/Application.Designer.vb
+++ Inventaris/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Inventaris.MasterTipe
+            Me.MainForm = Global.Inventaris.Form1
         End Sub
     End Class
 End Namespace
