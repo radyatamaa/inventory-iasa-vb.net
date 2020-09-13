6f916b9efa31b74f7684de1da188f867840f5dc0
bagusshndr <bagussuhendri19@gmail.com>
2020-09-13 23:21:53 +0700

    push laporan barang masuk,rental, dan penjualan

---
+++ Inventaris/DataSet2_Stock.vb
@@ -0,0 +1,8 @@
+﻿Partial Class DataSet2_Stock
+    Partial Public Class DataRentalDataTable
+        Private Sub DataRentalDataTable_DataRentalRowChanging(sender As Object, e As DataRentalRowChangeEvent) Handles Me.DataRentalRowChanging
+
+        End Sub
+
+    End Class
+End Class
