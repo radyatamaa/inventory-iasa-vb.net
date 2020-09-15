5a9c1b2923e5512f0fbb412a6bea36402d518de7
bagusshndr <bagussuhendri19@gmail.com>
2020-09-15 23:32:30 +0700

    Revert "push catatan kelima baguse"
    This reverts commit 61240a30d9eb86f31405e710afbb4ee0a74cf9f3.

--- Inventaris/PengembalianRental.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class PengembalianRental
-    Private Sub Label5_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
-
-    End Sub
-End Class
--- Inventaris/ReplacementBarang.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class ReplacementBarang
-
-End Class
